8b792a2504ca1facb8efa749449ff3317cf04ccd
Adam Czapliński <adam.czaplinski@vavatech.pl>
2020-01-23 20:32:22 +0100

    Validating whether all Factory Methods are inside Factories

--- src/main/java/org/jpatterns/validator/AbstractFactoryPatternValidator.java
+++ src/main/java/org/jpatterns/validator/AbstractFactoryPatternValidator.java
@@ -1,22 +1,29 @@
 package org.jpatterns.validator;
 
-import javax.annotation.processing.Completion;
-import javax.annotation.processing.ProcessingEnvironment;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
+import org.jpatterns.gof.creational.AbstractFactoryPattern;
+
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 import java.util.Collections;
 import java.util.Set;
 
 public class AbstractFactoryPatternValidator implements Processor {
+    private Elements elements;
+    private Messager messager;
+    private Types types;
 
     @Override
     public void init(ProcessingEnvironment processingEnv) {
-
+        elements = processingEnv.getElementUtils();
+        messager = processingEnv.getMessager();
+        types = processingEnv.getTypeUtils();
     }
 
     @Override
@@ -38,6 +45,22 @@ public class AbstractFactoryPatternValidator implements Processor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         System.out.println("PROCESSING " + annotations.size() + "!");
         System.out.println(annotations);
+        for(Element annotatedElement :
+                roundEnv.getElementsAnnotatedWith(AbstractFactoryPattern.AbstractFactory.class)) {
+        }
+        for(Element annotatedElement :
+                roundEnv.getElementsAnnotatedWith(AbstractFactoryPattern.AbstractProduct.class)) {
+        }
+        for(Element annotatedElement :
+                roundEnv.getElementsAnnotatedWith(AbstractFactoryPattern.ConcreteFactory.class)) {
+        }
+        for(Element annotatedElement :
+                roundEnv.getElementsAnnotatedWith(AbstractFactoryPattern.ConcreteProduct.class)) {
+        }
+        for(Element annotatedElement :
+                roundEnv.getElementsAnnotatedWith(AbstractFactoryPattern.FactoryMethod.class)) {
+            validateFactoryMethodIsInsideFactory(annotatedElement);
+        }
         return false;
     }
 
@@ -45,5 +68,25 @@ public class AbstractFactoryPatternValidator implements Processor {
     public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation,
                                                          ExecutableElement member, String userText) {
         return Collections.emptyList();
+    }
+
+    private void validateFactoryMethodIsInsideFactory(Element annotatedElement) {
+        if(annotatedElement.getEnclosingElement().getAnnotation(AbstractFactoryPattern.AbstractFactory.class)
+                == null
+            && annotatedElement.getEnclosingElement().getAnnotation(AbstractFactoryPattern.ConcreteFactory.class)
+                == null) {
+            messager.printMessage(Diagnostic.Kind.ERROR, "Factory Method must reside in a class or interface" +
+                    " annotated with either @AbstractFactory or @ConcreteFactory", annotatedElement,
+                    elements.getAllAnnotationMirrors(annotatedElement)
+                            .stream()
+                            .filter(annotationMirror -> annotationMirror
+                                    .getAnnotationType()
+                                    .toString()
+                                    .equals(AbstractFactoryPattern.FactoryMethod.class
+                                            .getName()
+                                            .replace('$', '.')))
+                            .findFirst()
+                            .orElseThrow(() -> new RuntimeException("This can never happen")));
+        }
     }
 }
