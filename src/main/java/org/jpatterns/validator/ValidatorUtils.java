46ecee9e1c42c3ec933f02df2e4ad4866987c5b3
Adam Czapliński <adam.czaplinski@vavatech.pl>
2020-03-17 11:43:57 +0100

    Annotate Java standard library - some Iterators and Aggregates

--- src/main/java/org/jpatterns/validator/ValidatorUtils.java
+++ src/main/java/org/jpatterns/validator/ValidatorUtils.java
@@ -22,12 +22,26 @@ public class ValidatorUtils {
     private Types types;
     private static final Map<String, Set<Class<? extends Annotation>>> hardcodedAnnotations =
             Stream.<Map.Entry<Class<?>, Set<Class<? extends Annotation>>>>of(
-            Map.entry(Iterator.class, Set.of(IteratorPattern.Iterator.class)),
-            Map.entry(AbstractCollection.class, Set.of(IteratorPattern.Aggregate.class))
-    ).collect(Collectors.toMap(
-            entry -> entry.getKey().getName(),
-            Map.Entry::getValue
-    ));
+                    Map.entry(Iterator.class, Set.of(IteratorPattern.Iterator.class)),
+                    Map.entry(ListIterator.class, Set.of(IteratorPattern.Iterator.class)),
+                    Map.entry(Spliterator.class, Set.of(IteratorPattern.Iterator.class)),
+                    Map.entry(Iterable.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(Collection.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(AbstractCollection.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(List.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(AbstractList.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(Set.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(AbstractSet.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(SortedSet.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(NavigableSet.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(Map.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(AbstractMap.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(SortedMap.class, Set.of(IteratorPattern.Aggregate.class)),
+                    Map.entry(NavigableMap.class, Set.of(IteratorPattern.Aggregate.class))
+            ).collect(Collectors.toMap(
+                    entry -> entry.getKey().getName(),
+                    Map.Entry::getValue
+            ));
 
     public ValidatorUtils(ProcessingEnvironment processingEnv){
         elements = processingEnv.getElementUtils();
